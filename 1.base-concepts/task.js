790630a33b560e37d55bdf66bc7b9ca9353e5bfc
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2022-05-10 20:05:56 +0500

    3 в

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,14 +19,37 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
   let arr = [percent, contribution, amount, date];
-  for (let i = 0; i > arr.length; i++) {
-    if (isNaN(arr[i])) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(NaN)) {
       parseInt(arr[i]);
     } else {
-      return `Параметр ${arr[i]} содержит неправильное значение ${arr[i]}`;
+      return `Параметр ${arr[i]} содержит неправильное значение ${arr[i]}`
     }
   }
-
+  /*if (percent === NaN) {
+    parseInt(percent);
+  }
+  else {
+    return `Параметр ${percent} содержит неправильное значение ${percent}`
+  }
+  if (contribution === NaN) {
+    parseInt(contribution);
+  }
+  else {
+    return `Параметр ${contribution} содержит неправильное значение ${contribution}`
+  }
+  if (amount === NaN) {
+    parseInt(amount);
+  }
+  else {
+    return `Параметр ${amount} содержит неправильное значение ${amount}`
+  }
+  if (date === NaN) {
+    parseInt(date);
+  }
+  else {
+    return `Параметр ${date} содержит неправильное значение ${date}`
+  }*/
 
 
   let debit = amount - contribution;
@@ -35,7 +58,8 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let P = percent / 100 / 12;
   let pay = debit * (P + (P / (((1 + P) ** date) - 1)));
   totalAmount = contribution + (pay * date);
-  totalAmount = parseInt(totalAmount).toFixed(2);
+  totalAmount = totalAmount.toFixed(2);
+  totalAmount = parseInt(totalAmount);
 
   // код для задачи №2 писать здесь
 

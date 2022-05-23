278685223e4661dffaec90dd6fe910d3150227b3
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2022-05-23 14:59:29 +0500

    1v

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = arr1.every((item, idx, arr) => arr1[i] === arr2[i]);
   return result; // boolean
 }
+// Ваш код
+
+
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item)s => item * 10)
 
   return resultArr; // array
 }

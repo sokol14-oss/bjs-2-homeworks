2da04fb67f32df7fde5a7a34c583ef3760bb4481
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2022-06-18 15:51:25 +0500

    3v

--- 7.async/task.js
+++ 7.async/task.js
@@ -7,19 +7,17 @@ class AlarmClock {
         if (!id) {
             throw new Error('Нужен Id');
         }
-        if (this.alarmCollection.some((item) => item.id === this.addClock.id)) {
+        if (this.alarmCollection.some((item) => item.id === id)) {
             console.error("Ошибка");
         }
-        this.alarmCollection.push({ time: time, callback: callback, id: id })
+        this.alarmCollection.push({ time, callback, id })
     }
 
 
     removeClock(id) {
         let alarmCollection1 = this.alarmCollection;
         this.alarmCollection = this.alarmCollection.filter((item) => item.id != id);
-        if (alarmCollection1.length != this.alarmCollection.length) {
-            return true
-        }
+        return alarmCollection1.length != this.alarmCollection.length;
     }
     getCurrentFormattedTime() {
         return new Date().toLocaleTimeString("ru", { hour: '2-digit', minute: '2-digit' });
@@ -28,23 +26,22 @@ class AlarmClock {
         if (this.timerId) {
             return;
         }
-        this.timerId = setInterval(check, 1000);
-        const checklock(time) => {
-            if (getCurrentFormattedTime() === this.alarmCollection.time) {
-                return this.callback;
+        this.timerId = setInterval(forEach(checkClock), 1000);
+        const checkClock(time) => {
+            if (this.getCurrentFormattedTime() === item.time) {
+                callback();
             }
-            const check = this.alarmCollection.forEach(checklock);
         }
 
     }
     stop() {
-        if (this.addClock.id === true) {
+        if (this.timerId) {
             clearInterval(this.timerId);
         }
         this.timerId = null;
     }
     printAlarms() {
-        console.log(this.alarmCollection.forEach((item) => this.item.id + this.item.time));
+        this.alarmCollection.forEach((item) => console.log(item.id, item.time));
     }
     clearAlarms() {
         this.stop();
